bde96ac13cb5098e815fc0a54e575b5ee3b7558f
RoachxD <roachxxxx@gmail.com>
2015-09-19 20:26:33 +0300

    First Release

---
+++ Item Swapper Buddy/Item Swapper Buddy/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+
+namespace Item_Swapper_Buddy
+{
+    internal class Program
+    {
+        private static int _firstKey = 0x60;
+        private static readonly int[] Keys = {0x64, 0x65, 0x66, 0x61, 0x62, 0x63};
+
+        // ReSharper disable once UnusedParameter.Local
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += delegate
+            {
+                var onLoadingComplete = new Thread(Loading_OnLoadingComplete);
+                onLoadingComplete.Start();
+            };
+        }
+
+        private static void Loading_OnLoadingComplete()
+        {
+            Chat.Print("Item Swapper Buddy - <font color=\"#FFFFFF\">Loaded</font>", Color.FromArgb(255, 210, 68, 74));
+
+            Game.OnWndProc += Game_OnWndProc;
+        }
+
+        private static void Game_OnWndProc(WndEventArgs args)
+        {
+            if (args.Msg == 0x0100 && args.WParam == 0x60)
+            {
+                _firstKey = 0x60;
+            }
+
+            if (args.Msg != 0x0100 || !Keys.ToList().Contains((byte) args.WParam))
+            {
+                return;
+            }
+
+            var key = (int) args.WParam;
+            if (_firstKey == 0x60)
+            {
+                _firstKey = key;
+            }
+
+            if (_firstKey == key)
+            {
+                return;
+            }
+
+			Chat.Print(":D");
+			
+            Player.SwapItem(Array.IndexOf(Keys, _firstKey), Array.IndexOf(Keys, key));
+            _firstKey = 0x60;
+        }
+    }
+}
---
+++ Item Swapper Buddy/Item Swapper Buddy/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Item Swapper Buddy")]
+[assembly: AssemblyDescription("Swaps Items using the Numpad.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EloBuddy")]
+[assembly: AssemblyProduct("Item Swapper Buddy")]
+[assembly: AssemblyCopyright("Copyright © Roach_ 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("ed3e81a9-d1ce-4618-aeb0-da65a0e38d1a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
