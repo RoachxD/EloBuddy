7f4ec7fabf6d103ec0f56270606f19a569399930
RoachxD <roachxxxx@gmail.com>
2015-09-19 21:37:22 +0300

    WIP

---
+++ Marksman Buddy/Marksman Buddy/Internal/Variables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+
+namespace Marksman_Buddy.Internal
+{
+    class Variables
+    {
+        public static Menu Config;
+        public static bool ComboMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Combo;
+        public static bool HarassMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Harass;
+        public static bool LaneClearMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LaneClear;
+        public static bool LastHitMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LastHit;
+    }
+}
--- Marksman Buddy/Marksman Buddy/Program.cs
+++ Marksman Buddy/Marksman Buddy/Program.cs
@@ -2,13 +2,16 @@
 using System.Globalization;
 using System.Threading;
 using EloBuddy;
+using EloBuddy.SDK;
 using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using Marksman_Buddy.Internal;
 
 namespace Marksman_Buddy
 {
     internal class Program
     {
-        public static Internal.Champion ChampionClass;
+        public static Internal.Champion ChampionPlugin;
         private static void Main(string[] args)
         {
             Loading.OnLoadingComplete += delegate
@@ -20,74 +23,81 @@ namespace Marksman_Buddy
 
         private static void Loading_OnLoadingComplete()
         {
+            Bootstrap.Init(null);
+            Variables.Config = MainMenu.AddMenu("Marksman Buddy", "MarksmanBuddy");
+            Variables.Config.AddGroupLabel("Marksman Buddy");
+            Variables.Config.AddLabel("Version: " + "1.0.0.0");
+            Variables.Config.AddSeparator();
+            Variables.Config.AddLabel("Creators: " + "Roach, newchild");
+
+
             Chat.Print("Marksman Buddy - <font color=\"#FFFFFF\">Loaded</font>", Color.FromArgb(255, 210, 68, 74));
 
             var championName = ObjectManager.Player.ChampionName.ToLower(CultureInfo.InvariantCulture);
-
             switch (championName)
             {
                 case "ashe":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "caitlyn":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "corki":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "draven":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "ezreal":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "graves":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "gnar":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "jinx":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "kalista":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "kindred":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "kogmaw":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "lucian":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "missfortune":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "quinn":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "sivir":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "teemo":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "tristana":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "twitch":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "urgot":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "vayne":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
                 case "varus":
-                    ChampionClass = new Internal.Champion();
+                    ChampionPlugin = new Internal.Champion();
                     break;
             }
         }
