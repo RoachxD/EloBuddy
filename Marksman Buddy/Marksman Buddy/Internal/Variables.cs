803ec7f16820cef73d0adb077bcb7dc47a5fde63
newchild <fabianbrand98@gmx.de>
2015-09-19 22:59:05 +0200

    Global prediction

--- Marksman Buddy/Marksman Buddy/Internal/Variables.cs
+++ Marksman Buddy/Marksman Buddy/Internal/Variables.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
 using EloBuddy.SDK.Menu;
 
 namespace Marksman_Buddy.Internal
@@ -10,9 +11,11 @@ namespace Marksman_Buddy.Internal
     class Variables
     {
         public static Menu Config;
+		public static Menu Settings;
         public static bool ComboMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Combo;
         public static bool HarassMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Harass;
         public static bool LaneClearMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LaneClear;
         public static bool LastHitMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LastHit;
+		public static HitChance Hitchance = HitChance.High;
     }
 }
