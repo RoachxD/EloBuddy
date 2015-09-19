15186a1563cf8a6ef053ef4fbe9dc6f19d99b32f
RoachxD <roachxxxx@gmail.com>
2015-09-20 01:12:16 +0300

    A few changes

--- Marksman Buddy/Marksman Buddy/Plugins/Corki.cs
+++ Marksman Buddy/Marksman Buddy/Plugins/Corki.cs
@@ -1,123 +1,133 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using EloBuddy.SDK;
-using EloBuddy.SDK.Events;
-using EloBuddy.SDK.Menu;
-using EloBuddy.SDK.Menu.Values;
 using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu.Values;
 using Marksman_Buddy.Internal;
 
 namespace Marksman_Buddy.Plugins
 {
-	class Corki: PluginBase
-	{
-		private Spell.Active _E = new Spell.Active(SpellSlot.E, 600);
-		private Spell.Skillshot _Q = new Spell.Skillshot(SpellSlot.Q, 825, EloBuddy.SDK.Enumerations.SkillShotType.Circular, 300, 1000, 250);
-		private Spell.Skillshot _W = new Spell.Skillshot(SpellSlot.W, 800, EloBuddy.SDK.Enumerations.SkillShotType.Linear);
-		private Spell.Skillshot _R1 = new Spell.Skillshot(SpellSlot.R, 1300, EloBuddy.SDK.Enumerations.SkillShotType.Linear, 200, 2000, 40);
-		private int[] _RDamage = new int[] { 100, 180, 260};
-		private float[] _RDamageScale = new float[] { 0.2f, 0.3f, 0.4f };
-		//private Spell.Skillshot _R2 = new Spell.Skillshot(SpellSlot.R, 1500, EloBuddy.SDK.Enumerations.SkillShotType.Linear, 200, 2000, 40);
+    internal class Corki : PluginBase
+    {
+        private readonly Spell.Skillshot _Q = new Spell.Skillshot(SpellSlot.Q, 825, SkillShotType.Circular, 300, 1000,
+            250);
 
-		public Corki()
-		{
-			_SetupMenu();
-			Game.OnTick += Game_OnTick;
-			Gapcloser.OnGapCloser += Gapcloser_OnGapCloser;
-		}
+        private readonly Spell.Skillshot _R1 = new Spell.Skillshot(SpellSlot.R, 1300, SkillShotType.Linear, 200, 2000,
+            40);
 
-		void Gapcloser_OnGapCloser(AIHeroClient sender, Gapcloser.GapCloserEventArgs e)
-		{
-			if (Variables.Config["useWAntigapcloser"].Cast<CheckBox>().CurrentValue)
-			{
-				if(sender.Distance(ObjectManager.Player) < 1400
-					&& (e.End.Distance(ObjectManager.Player) < sender.Distance(ObjectManager.Player)))
-				{
-					_W.Cast((ObjectManager.Player.Position.Extend(e.End, -1 * _W.Range)).To3D());
-				}
-			}
-		}
+        private readonly int[] _RDamage = {100, 180, 260};
+        private readonly float[] _RDamageScale = {0.2f, 0.3f, 0.4f};
+        private readonly Spell.Skillshot _W = new Spell.Skillshot(SpellSlot.W, 800, SkillShotType.Linear);
+        private Spell.Active _E = new Spell.Active(SpellSlot.E, 600);
+        //private Spell.Skillshot _R2 = new Spell.Skillshot(SpellSlot.R, 1500, EloBuddy.SDK.Enumerations.SkillShotType.Linear, 200, 2000, 40);
 
-		private void Game_OnTick(EventArgs args)
-		{
-			if (Variables.ComboMode)
-			{
-				_Combo();
-			}
-			if (Variables.HarassMode)
-			{
-				_Harrass();
-			}
-			_KS();
-		}
+        public Corki()
+        {
+            _SetupMenu();
+            Game.OnTick += Game_OnTick;
+            Gapcloser.OnGapCloser += Gapcloser_OnGapCloser;
+        }
 
-		private void _KS()
-		{
-			foreach(var hero in
-				HeroManager.Enemies
-					.Where(x => x.Position.Distance(ObjectManager.Player) < _R1.Range)){
-						if (_RCanKill(hero, _R1.Level) && Variables.Config["useRKS"].Cast<CheckBox>().CurrentValue)
-						{
-							_R1.Cast(hero);
-						}
-			}
-		}
+        private void Gapcloser_OnGapCloser(AIHeroClient sender, Gapcloser.GapCloserEventArgs e)
+        {
+            if (!Variables.Config["useWAntigapcloser"].Cast<CheckBox>().CurrentValue)
+            {
+                return;
+            }
 
-		private void _Harrass()
-		{
-			var QTarget = TargetSelector.GetTarget(_Q.Range, DamageType.Magical);
-			var RTarget = TargetSelector.GetTarget(_R1.Range, DamageType.Magical);
-			if (QTarget.IsValidTarget() && Variables.Config["useQHarass"].Cast<CheckBox>().CurrentValue)
-			{
-				_Q.Cast(QTarget);
-			}
-			if (RTarget.IsValidTarget() && Variables.Config["useRHarass"].Cast<CheckBox>().CurrentValue
-				&& Variables.Config["useRHarassStacks"].Cast<Slider>().CurrentValue < _R1.Handle.Ammo)
-			{
-				_R1.Cast(RTarget);
-			}
-		}
+            if (sender.Distance(ObjectManager.Player) < 1400
+                && (e.End.Distance(ObjectManager.Player) < sender.Distance(ObjectManager.Player)))
+            {
+                _W.Cast((ObjectManager.Player.Position.Extend(e.End, -1*_W.Range)).To3D());
+            }
+        }
 
-		private bool _RCanKill(Obj_AI_Base target, int Level)
-		{
-			float EDamage =
-				_RDamage[Level] +
-				ObjectManager.Player.TotalAttackDamage * _RDamageScale[Level]
-				+ ObjectManager.Player.TotalMagicalDamage * 0.3f - 20.0f; //Damage Calc is off
-			return Damage.CalculateDamageOnUnit(ObjectManager.Player, target, DamageType.Magical, EDamage) > target.Health;
-		}
+        private void Game_OnTick(EventArgs args)
+        {
+            if (Variables.ComboMode)
+            {
+                _Combo();
+            }
 
-		private void _Combo()
-		{
-			var QTarget = TargetSelector.GetTarget(_Q.Range, DamageType.Magical);
-			var RTarget = TargetSelector.GetTarget(_R1.Range, DamageType.Magical);
-			if (QTarget.IsValidTarget() && Variables.Config["useQCombo"].Cast<CheckBox>().CurrentValue)
-			{
-				_Q.Cast(QTarget);
-			}
-			if (RTarget.IsValidTarget() && Variables.Config["useRCombo"].Cast<CheckBox>().CurrentValue
-				&& Variables.Config["useRComboStacks"].Cast<Slider>().CurrentValue < _R1.Handle.Ammo)
-			{
-				_R1.Cast(RTarget);
-			}
-		}
+            if (Variables.HarassMode)
+            {
+                _Harrass();
+            }
 
-		private void _SetupMenu()
-		{
-			Variables.Config.AddGroupLabel("Corki");
-			Variables.Config.AddGroupLabel("Combo");
-			Variables.Config.Add("useQCombo", new CheckBox("Use Q in Combo"));
-			Variables.Config.Add("useRCombo", new CheckBox("Use R in Combo"));
-			Variables.Config.Add("useRComboStacks", new Slider("Save x Rockets", 3, 0, 7));
-			Variables.Config.AddGroupLabel("Harass");
-			Variables.Config.Add("useQHarass", new CheckBox("Use Q in Harass", false));
-			Variables.Config.Add("useRHarass", new CheckBox("Use R in Harass"));
-			Variables.Config.Add("useRHarassStacks", new Slider("Save x Rockets", 5, 0, 7));
-			Variables.Config.AddGroupLabel("Misc");
-			Variables.Config.Add("useWAntigapcloser", new CheckBox("Use W upon Gapcloser", false));
-			Variables.Config.Add("useRKS", new CheckBox("Use R to KS (ignoring Stack limitation)"));
+            _KS();
+        }
 
-		}
-	}
+        private void _KS()
+        {
+            foreach (var hero in
+                HeroManager.Enemies
+                    .Where(x => x.Position.Distance(ObjectManager.Player) < _R1.Range))
+            {
+                if (_RCanKill(hero, _R1.Level) && Variables.Config["useRKS"].Cast<CheckBox>().CurrentValue)
+                {
+                    _R1.Cast(hero);
+                }
+            }
+        }
+
+        private void _Harrass()
+        {
+            var QTarget = TargetSelector.GetTarget(_Q.Range, DamageType.Magical);
+            var RTarget = TargetSelector.GetTarget(_R1.Range, DamageType.Magical);
+            if (QTarget.IsValidTarget() && Variables.Config["useQHarass"].Cast<CheckBox>().CurrentValue)
+            {
+                _Q.Cast(QTarget);
+            }
+
+            if (RTarget.IsValidTarget() && Variables.Config["useRHarass"].Cast<CheckBox>().CurrentValue
+                && Variables.Config["useRHarassStacks"].Cast<Slider>().CurrentValue < _R1.Handle.Ammo)
+            {
+                _R1.Cast(RTarget);
+            }
+        }
+
+        private bool _RCanKill(Obj_AI_Base target, int Level)
+        {
+            var EDamage =
+                _RDamage[Level] +
+                ObjectManager.Player.TotalAttackDamage*_RDamageScale[Level]
+                + ObjectManager.Player.TotalMagicalDamage*0.3f - 20.0f; //Damage Calc is off
+
+            return ObjectManager.Player.CalculateDamageOnUnit(target, DamageType.Magical, EDamage) > target.Health;
+        }
+
+        private void _Combo()
+        {
+            var QTarget = TargetSelector.GetTarget(_Q.Range, DamageType.Magical);
+            var RTarget = TargetSelector.GetTarget(_R1.Range, DamageType.Magical);
+            if (QTarget.IsValidTarget() && Variables.Config["useQCombo"].Cast<CheckBox>().CurrentValue)
+            {
+                _Q.Cast(QTarget);
+            }
+
+            if (RTarget.IsValidTarget() && Variables.Config["useRCombo"].Cast<CheckBox>().CurrentValue
+                && Variables.Config["useRComboStacks"].Cast<Slider>().CurrentValue < _R1.Handle.Ammo)
+            {
+                _R1.Cast(RTarget);
+            }
+        }
+
+        private void _SetupMenu()
+        {
+            Variables.Config.AddGroupLabel("Corki");
+            Variables.Config.AddGroupLabel("Combo");
+            Variables.Config.Add("useQCombo", new CheckBox("Use Q in Combo"));
+            Variables.Config.Add("useRCombo", new CheckBox("Use R in Combo"));
+            Variables.Config.Add("useRComboStacks", new Slider("Save x Rockets", 3, 0, 7));
+            Variables.Config.AddGroupLabel("Harass");
+            Variables.Config.Add("useQHarass", new CheckBox("Use Q in Harass", false));
+            Variables.Config.Add("useRHarass", new CheckBox("Use R in Harass"));
+            Variables.Config.Add("useRHarassStacks", new Slider("Save x Rockets", 5, 0, 7));
+            Variables.Config.AddGroupLabel("Misc");
+            Variables.Config.Add("useWAntigapcloser", new CheckBox("Use W upon Gapcloser", false));
+            Variables.Config.Add("useRKS", new CheckBox("Use R to KS (ignoring Stack limitation)"));
+        }
+    }
 }
--- Marksman Buddy/Marksman Buddy/Plugins/Twitch.cs
+++ Marksman Buddy/Marksman Buddy/Plugins/Twitch.cs
@@ -1,167 +1,162 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using EloBuddy.SDK;
-using EloBuddy.SDK.Menu;
-using EloBuddy.SDK.Menu.Values;
 using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Menu.Values;
 using Marksman_Buddy.Internal;
 
 namespace Marksman_Buddy.Plugins
 {
-	class Twitch : Internal.PluginBase
-	{
-		private static Spell.Skillshot _W;
-		private static Spell.Active _E;
-		private static int[] _EDamage = new int[] { 20, 35, 50, 65, 80 };
-		private static string[] _Minions = new string[] { "SRU_Dragon", "SRU_Baron", "Sru_Crab", "Siege" };
+    internal class Twitch : PluginBase
+    {
+        private static Spell.Skillshot _W;
+        private static Spell.Active _E;
+        private static readonly int[] _EDamage = {20, 35, 50, 65, 80};
+        private static readonly string[] _Minions = {"SRU_Dragon", "SRU_Baron", "Sru_Crab", "Siege"};
 
-		public Twitch()
-		{
-			_SetupMenu();
-			_SetupSpells();
-			Game.OnTick += Game_OnTick;
-		}
+        public Twitch()
+        {
+            _SetupMenu();
+            _SetupSpells();
+            Game.OnTick += Game_OnTick;
+        }
 
-		private static void _SetupSpells()
-		{
-			_W = new Spell.Skillshot(SpellSlot.W, 900, EloBuddy.SDK.Enumerations.SkillShotType.Circular, 250, 1400, 275);
-			_E = new Spell.Active(SpellSlot.E, 1200);
-		}
+        private static void _SetupSpells()
+        {
+            _W = new Spell.Skillshot(SpellSlot.W, 900, SkillShotType.Circular, 250, 1400, 275);
+            _E = new Spell.Active(SpellSlot.E, 1200);
+        }
 
-		private static void _SetupMenu()
-		{
-			Variables.Config.AddGroupLabel("Twitch");
-			Variables.Config.AddGroupLabel("Combo");
-			Variables.Config.Add("Twitch.UseECombo", new CheckBox("Use E in Combo"));
-			Variables.Config.Add("Twitch.UseEComboStacks", new Slider("Cast E at x Stacks", 5, 1, 5));
-			Variables.Config.Add("Twitch.UseWCombo", new CheckBox("Use W in Combo"));
-			Variables.Config.AddGroupLabel("Harrass");
-			Variables.Config.Add("Twitch.UseEHarass", new CheckBox("Use E in Harass"));
-			Variables.Config.Add("Twitch.UseEHarassStacks", new Slider("Cast E at x Stacks", 3, 1, 5));
-			Variables.Config.Add("Twitch.UseWHarass", new CheckBox("Use W in Harass", false));
-			Variables.Config.AddGroupLabel("Misc");
-			Variables.Config.Add("Twitch.KS", new CheckBox("Use E to KS"));
-			Variables.Config.Add("Twitch.EExecute", new CheckBox("Use E to execute Large Minions")); //Kappa
-		}
+        private static void _SetupMenu()
+        {
+            Variables.Config.AddGroupLabel("Twitch");
+            Variables.Config.AddGroupLabel("Combo");
+            Variables.Config.Add("Twitch.UseECombo", new CheckBox("Use E in Combo"));
+            Variables.Config.Add("Twitch.UseEComboStacks", new Slider("Cast E at x Stacks", 5, 1, 5));
+            Variables.Config.Add("Twitch.UseWCombo", new CheckBox("Use W in Combo"));
+            Variables.Config.AddGroupLabel("Harrass");
+            Variables.Config.Add("Twitch.UseEHarass", new CheckBox("Use E in Harass"));
+            Variables.Config.Add("Twitch.UseEHarassStacks", new Slider("Cast E at x Stacks", 3, 1, 5));
+            Variables.Config.Add("Twitch.UseWHarass", new CheckBox("Use W in Harass", false));
+            Variables.Config.AddGroupLabel("Misc");
+            Variables.Config.Add("Twitch.KS", new CheckBox("Use E to KS"));
+            Variables.Config.Add("Twitch.EExecute", new CheckBox("Use E to execute Large Minions")); //Kappa
+        }
 
-		static void Game_OnTick(EventArgs args)
-		{
-			if (Variables.ComboMode)
-			{
-				_Combo();
-			}
-			if (Variables.HarassMode)
-			{
-				_Harrass();
-			}
-			_KS();
-			_Execute();
-		}
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Variables.ComboMode)
+            {
+                _Combo();
+            }
 
-		private static void _Execute()
-		{
-			foreach (var minion in 
-				ObjectManager.Get<Obj_AI_Minion>()
-					.Where(x => x.Position.Distance(ObjectManager.Player.Position) < 1200))
-			{
-				if (minion.Team != ObjectManager.Player.Team &&
-						_ContainsSimiliar(_Minions, minion.Name))
-				{
-					if (_ECanKill(minion, _E) &&
-						Variables.Config["Twitch.EExecute"].Cast<CheckBox>().CurrentValue)
-					{
-						_E.Cast();
-					}
-						
-				}
-			}
-		}
+            if (Variables.HarassMode)
+            {
+                _Harrass();
+            }
 
-		private static bool _ContainsSimiliar(string[] array, string simily)//Who needs comparators Kappa
-		{
-			foreach (string element in array)
-			{
-				if (element.ToLower().Contains(simily.ToLower()))
-					return true;
-			}
-			return false;
-		}
+            _KillSteal();
+            _Execute();
+        }
 
-		private static void _Harrass()
-		{
-			var WTarget = TargetSelector.GetTarget(_W.Range, DamageType.True);
-			if (Variables.Config["Twitch.UseWHarass"].Cast<CheckBox>().CurrentValue
-				&& !_W.IsOnCooldown)
-			{
-				_W.Cast(WTarget);
-			}
+        private static void _Execute()
+        {
+            foreach (var minion in 
+                ObjectManager.Get<Obj_AI_Minion>()
+                    .Where(x => x.Position.Distance(ObjectManager.Player.Position) < 1200))
+            {
+                if (minion.Team == ObjectManager.Player.Team || !_ContainsSimiliar(_Minions, minion.Name))
+                {
+                    continue;
+                }
 
-			if (!Variables.Config["Twitch.UseEHarass"].Cast<CheckBox>().CurrentValue)
-				return;
-			foreach (var Hero in
-				ObjectManager.Get<AIHeroClient>()
-					.Where(x => x.Position.Distance(ObjectManager.Player.Position) < 1200))
-			{
-				if (Hero.GetBuffCount("twitchdeadlyvenom") >=
-					Variables.Config["Twitch.UseEHarassStacks"].Cast<Slider>().CurrentValue)
-				{
-					_E.Cast();
-				}
-			}
-		}
+                if (_ECanKill(minion, _E) &&
+                    Variables.Config["Twitch.EExecute"].Cast<CheckBox>().CurrentValue)
+                {
+                    _E.Cast();
+                }
+            }
+        }
 
-		private static void _KS()
-		{
-			foreach (var Hero in
-				ObjectManager.Get<AIHeroClient>()
-					.Where(x => x.Position.Distance(ObjectManager.Player.Position) < 1200))
-			{
-				if (_ECanKill(Hero, _E) && Variables.Config["Twitch.KS"].Cast<CheckBox>().CurrentValue)
-				{
-					_E.Cast();
-				}
-					
-			}
-		}
+        private static bool _ContainsSimiliar(string[] array, string simily) //Who needs comparators Kappa
+        {
+            return array.Any(element => element.ToLower().Contains(simily.ToLower()));
+        }
 
+        private static void _Harrass()
+        {
+            var WTarget = TargetSelector.GetTarget(_W.Range, DamageType.True);
+            if (Variables.Config["Twitch.UseWHarass"].Cast<CheckBox>().CurrentValue
+                && !_W.IsOnCooldown)
+            {
+                _W.Cast(WTarget);
+            }
 
+            if (!Variables.Config["Twitch.UseEHarass"].Cast<CheckBox>().CurrentValue)
+            {
+                return;
+            }
 
-		private static void _Combo()
-		{
-			var WTarget = TargetSelector.GetTarget(_W.Range, DamageType.True);
-			if (Variables.Config["Twitch.UseWCombo"].Cast<CheckBox>().CurrentValue
-				&& !_W.IsOnCooldown)
-			{
-				_W.Cast(WTarget);
-			}
+            foreach (var hero in
+                ObjectManager.Get<AIHeroClient>()
+                    .Where(x => x.Position.Distance(ObjectManager.Player.Position) < 1200))
+            {
+                if (hero.GetBuffCount("twitchdeadlyvenom") >=
+                    Variables.Config["Twitch.UseEHarassStacks"].Cast<Slider>().CurrentValue)
+                {
+                    _E.Cast();
+                }
+            }
+        }
 
-			if (!Variables.Config["Twitch.UseECombo"].Cast<CheckBox>().CurrentValue)
-			{
-				return;
-			}
-				
-			foreach (var Hero in
-				ObjectManager.Get<AIHeroClient>()
-					.Where(x => x.Position.Distance(ObjectManager.Player.Position) < 1200))
-			{
-				if (Hero.GetBuffCount("twitchdeadlyvenom") >=
-					Variables.Config["Twitch.UseEComboStacks"].Cast<Slider>().CurrentValue)
-				{
-					_E.Cast();
-				}
-			}
+        private static void _KillSteal()
+        {
+            foreach (var hero in
+                ObjectManager.Get<AIHeroClient>()
+                    .Where(x => x.Position.Distance(ObjectManager.Player.Position) < 1200))
+            {
+                if (_ECanKill(hero, _E) && Variables.Config["Twitch.KS"].Cast<CheckBox>().CurrentValue)
+                {
+                    _E.Cast();
+                }
+            }
+        }
 
-		}
+        private static void _Combo()
+        {
+            var WTarget = TargetSelector.GetTarget(_W.Range, DamageType.True);
+            if (Variables.Config["Twitch.UseWCombo"].Cast<CheckBox>().CurrentValue
+                && !_W.IsOnCooldown)
+            {
+                _W.Cast(WTarget);
+            }
 
-		private static bool _ECanKill(Obj_AI_Base Hero, Spell.Active _E)
-		{
-			float EDamage = Convert.ToSingle(Hero.GetBuffCount("twitchdeadlyvenom") *
-				(_EDamage[_E.Level] +
-				ObjectManager.Player.TotalAttackDamage * 0.25
-				+ ObjectManager.Player.TotalMagicalDamage * 0.2)) - 20.0f; //Damage Calc is off
-			return Damage.CalculateDamageOnUnit(ObjectManager.Player, Hero, DamageType.Physical, EDamage) > Hero.Health;
+            if (!Variables.Config["Twitch.UseECombo"].Cast<CheckBox>().CurrentValue)
+            {
+                return;
+            }
 
-		}
-	}
+            foreach (var hero in
+                ObjectManager.Get<AIHeroClient>()
+                    .Where(x => x.Position.Distance(ObjectManager.Player.Position) < 1200))
+            {
+                if (hero.GetBuffCount("twitchdeadlyvenom") >=
+                    Variables.Config["Twitch.UseEComboStacks"].Cast<Slider>().CurrentValue)
+                {
+                    _E.Cast();
+                }
+            }
+        }
+
+        private static bool _ECanKill(Obj_AI_Base Hero, Spell.Active _E)
+        {
+            var EDamage = Convert.ToSingle(Hero.GetBuffCount("twitchdeadlyvenom")*
+                                           (_EDamage[_E.Level] +
+                                            ObjectManager.Player.TotalAttackDamage*0.25
+                                            + ObjectManager.Player.TotalMagicalDamage*0.2)) - 20.0f; //Damage Calc is off
+
+            return ObjectManager.Player.CalculateDamageOnUnit(Hero, DamageType.Physical, EDamage) > Hero.Health;
+        }
+    }
 }
