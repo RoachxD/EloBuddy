ef047ce4c29c3fa6c3db825119d7eb76d22e1a05
RoachxD <roachxxxx@gmail.com>
2015-09-26 14:23:59 +0300

    First Release

---
+++ In-Game Settings Buddy/In-Game Settings Buddy/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace In_Game_Settings_Buddy
+{
+    internal class Program
+    {
+        private static Menu _config;
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Loading.OnLoadingComplete += delegate
+                {
+                    var onLoadingComplete = new Thread(Loading_OnLoadingComplete);
+                    onLoadingComplete.Start();
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static void Loading_OnLoadingComplete()
+        {
+            _config = MainMenu.AddMenu("In-Game Settings Buddy", "ISB");
+
+            _config.Add("AntiAFK", new KeyBind("Enable Anti AFK", Hacks.AntiAFK, KeyBind.BindTypes.PressToggle, 112));
+            _config.Add("ExtendedZoom",
+                new KeyBind("Enable Extended Zoom", Hacks.ZoomHack, KeyBind.BindTypes.PressToggle, 113));
+            _config.Add("MovementHack",
+                new KeyBind("Enable Movement Hack", Hacks.MovementHack, KeyBind.BindTypes.PressToggle, 114));
+            _config.Add("Watermark",
+                new KeyBind("Draw Watermark", Hacks.RenderWatermark, KeyBind.BindTypes.PressToggle, 115));
+
+            Chat.Print("In-Game Settings: <font color=\"#FFFFFF\">Loaded</font>", Color.FromArgb(255, 210, 68, 74));
+
+            Game.OnTick += Game_OnTick;
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            Hacks.AntiAFK = _config["AntiAFK"].Cast<KeyBind>().CurrentValue;
+            Hacks.ZoomHack = _config["ExtendedZoom"].Cast<KeyBind>().CurrentValue;
+            Hacks.MovementHack = _config["MovementHack"].Cast<KeyBind>().CurrentValue;
+            Hacks.RenderWatermark = _config["Watermark"].Cast<KeyBind>().CurrentValue;
+        }
+    }
+}
---
+++ In-Game Settings Buddy/In-Game Settings Buddy/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("In-Game Settings Buddy")]
+[assembly: AssemblyDescription("It helps you change Loader Settings In-Game.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EloBuddy")]
+[assembly: AssemblyProduct("In-Game Settings Buddy")]
+[assembly: AssemblyCopyright("Copyright © Roach_ 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("378e9416-f792-438c-925c-eee78c1753ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
