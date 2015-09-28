a1869f2de22770addac7a61af1c88a7886d5443f
RoachxD <roachxxxx@gmail.com>
2015-09-28 19:43:25 +0300

    Update 1.0.0.2  - Added a Slider to set the Zoom Value.

--- In-Game Settings Buddy/In-Game Settings Buddy/Program.cs
+++ In-Game Settings Buddy/In-Game Settings Buddy/Program.cs
@@ -35,6 +35,7 @@ namespace In_Game_Settings_Buddy
             _config.Add("AntiAFK", new KeyBind("Enable Anti AFK", Hacks.AntiAFK, KeyBind.BindTypes.PressToggle, 112));
             _config.Add("ExtendedZoom",
                 new KeyBind("Enable Extended Zoom", Hacks.ZoomHack, KeyBind.BindTypes.PressToggle, 113));
+            _config.Add("ExtendedZoomValue", new Slider("Set the max Zoom Value:", 2250, 2250, 5000));
             _config.Add("MovementHack",
                 new KeyBind("Enable Movement Hack", Hacks.MovementHack, KeyBind.BindTypes.PressToggle, 114));
             _config.Add("InGameChat",
@@ -45,6 +46,8 @@ namespace In_Game_Settings_Buddy
             Chat.Print("In-Game Settings: <font color=\"#FFFFFF\">Loaded</font>", Color.FromArgb(255, 210, 68, 74));
 
             Game.OnTick += Game_OnTick;
+            _config["ExtendedZoom"].Cast<KeyBind>().OnValueChange += ExtendedZoom_OnValueChange;
+            _config["ExtendedZoomValue"].Cast<Slider>().OnValueChange += ExtendedZoomValue_OnValueChange;
         }
 
         private static void Game_OnTick(EventArgs args)
@@ -54,6 +57,21 @@ namespace In_Game_Settings_Buddy
             Hacks.MovementHack = _config["MovementHack"].Cast<KeyBind>().CurrentValue;
             Hacks.IngameChat = _config["InGameChat"].Cast<KeyBind>().CurrentValue;
             Hacks.RenderWatermark = _config["Watermark"].Cast<KeyBind>().CurrentValue;
+        }
+
+        private static void ExtendedZoom_OnValueChange(ValueBase<bool> sender, ValueBase<bool>.ValueChangeArgs args)
+        {
+            Camera.SetZoomDistance(args.NewValue ? _config["ExtendedZoomValue"].Cast<Slider>().CurrentValue : 2250);
+        }
+
+        private static void ExtendedZoomValue_OnValueChange(ValueBase<int> sender, ValueBase<int>.ValueChangeArgs args)
+        {
+            if (!_config["ExtendedZoom"].Cast<KeyBind>().CurrentValue)
+            {
+                return;
+            }
+
+            Camera.SetZoomDistance(args.NewValue);
         }
     }
 }
