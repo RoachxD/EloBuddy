07cb3edf6c73479efb84e27e2205602bf86dbdff
iRaxe <djvinx@live.com>
2015-12-30 23:45:27 +0100

    Added new code

---
+++ Marksman Buddy/Champions/Lucian.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarksmanAIO.Champions
+{
+    class Lucian : AIOChampion
+    {
+        public override void Combo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Marksman Buddy/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarksmanAIO.Champions
+{
+    class MainMenu
+    {
+        public static void Init()
+        {
+            //TODO: Main menu stuff n things
+        }
+    }
+}
